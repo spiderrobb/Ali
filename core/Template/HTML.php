dd5ce431afe2c7a05f3d895879e5e0ed060f7325
Robbin Harris <spiderrobb@gmail.com>
2015-09-10 07:32:00 -0700

    updated core html template with generic tempate vars

--- core/Template/HTML.php
+++ core/Template/HTML.php
@@ -5,7 +5,7 @@ use Ali\Base\TemplateAbstract;
 
 class HTML extends TemplateAbstract {
 	public function build($args) {
-		$args['html_content'] = $this->_view('Template', $args, true);
+		$args['content'] = $this->_view('Template', $args, true);
 		parent::build($args);
 	}
 }
