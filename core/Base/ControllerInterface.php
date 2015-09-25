03e8ba2e5b5eedd5621621904e115791b28fd356
Robbin Harris <spiderrobb@gmail.com>
2015-09-25 00:21:06 -0700

    removed required index method and added interface fore get absolute url function

--- core/Base/ControllerInterface.php
+++ core/Base/ControllerInterface.php
@@ -3,12 +3,12 @@ namespace Ali\Base;
 use Ali\Input;
 interface ControllerInterface {
 	public function __construct(UserInterface $user, Input $input);
-	public function actionIndex();
 	public function init();
 	public function getTitle($method = null);
 	public function getTemplate($method = null);
 	public static function getRoles($method = null);
 	public static function getPermissions($method = null);
 	public static function getURL($method = null, array $get = array(), array $args = array());
+	public static function getAbsoluteURL($method = null, array $get = array(), array $args = array());
 }
 ?>
